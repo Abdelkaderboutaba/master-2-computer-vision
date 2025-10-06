e3b7d2da681d1c78be57bf2c413d7866e782012d
abdelkader <boutaba.abdelkader1@gmail.com>
2025-10-06 23:09:25 +0100

    create visualisation using d3

---
+++ visualisation-de-donnees/tp1/Example1/index.js
@@ -0,0 +1,101 @@
+const main = (container) => {
+    const svg = d3.select(container)
+                  .selectAll('svg')
+                  .data([null])
+                  .join('svg')
+                  .attr('width', 900)
+                  .attr('height', 500);
+    svg.style('background-color', 'grey');
+
+    const width = +svg.attr('width');
+    const height = +svg.attr('height');
+
+    const faceGroup = svg.append('g')
+                      .attr('transform', "translate(450, 250)");
+    
+    faceGroup.append("circle")
+             .attr("cx",50)
+             .attr("cy",40)
+             .attr("r",40)
+             .style("fill","yellow")
+             .attr("stroke","black")
+    
+    const eyesgroupe=faceGroup.append("g")
+                              .attr("transform","translate (50,30)")
+
+    const lefteyesgroupe=eyesgroupe.append("g")
+                               .attr("transform" , "translate(-15,0)")
+    
+    lefteyesgroupe.append("circle")
+              .attr("r",5)
+              .attr("fill","blue")
+    
+    const righteyegroupe=eyesgroupe.append("g")
+                                   .attr("transform" , "translate(15,0)")
+
+
+
+    righteyegroupe.append("circle")
+                  .attr("r",5)
+                  .attr("fill","blue")
+    righteyegroupe.append("line")
+                  .attr("x1",-7)
+                  .attr("y1",-7)
+                  .attr("x2",7)
+                  .attr("y2",-7)
+                  .attr("stroke","black")
+                
+    lefteyesgroupe.append("line")
+                  .attr("x1",-7)
+                  .attr("y1",-7)
+                  .attr("x2",7)
+                  .attr("y2",-7)
+                  .attr("stroke","black")
+    
+
+
+
+    // const face = faceGroup.append('circle');
+    // face.attr('stroke', 'black')
+    //     .attr('stroke-width', '10')
+    //     .attr('r', height/2 - face.attr('stroke-width'))    
+    //     .attr('fill', 'yellow');
+
+//     const eyesGroup = faceGroup.append('g')
+//                                 .attr('transform', 'translate(0, -60)');
+
+//     const leftEye = eyesGroup.append('circle')
+//                             .attr('r', 30)
+//                             .attr('cx', -80);
+        
+//     const rightEye = eyesGroup.append('circle')
+//                             .attr('r', 30)
+//                             .attr('cx', 80);
+
+//     const leftEyeBrow = eyesGroup.append('line')
+//                             .attr('x1', +leftEye.attr('cx')-50)
+//                             .attr('y1', -80)
+//                             .attr('x2', +leftEye.attr('cx')+50)
+//                             .attr('y2', -80)
+//                             .attr('stroke-width', '5')
+//                             .attr('stroke', 'black');
+
+//     const rightEyeBrow = eyesGroup.append('line')
+//                             .attr('x1', +rightEye.attr('cx')-50)
+//                             .attr('y1', -80)
+//                             .attr('x2', +rightEye.attr('cx')+50)
+//                             .attr('y2', -80)
+//                             .attr('stroke-width', '5')
+//                             .attr('stroke', 'black');
+
+//     const mouth_arc = d3.arc()
+//                         .innerRadius(+face.attr('r')-60)
+//                         .outerRadius(+face.attr('r')-70)
+//                         .startAngle(Math.PI/2)
+//                         .endAngle(3*Math.PI/2);
+
+//     const mouth = faceGroup.append('path')
+//                             .attr('d', mouth_arc()); 
+}
+const container = document.getElementById('container');
+main(container);
---
+++ visualisation-de-donnees/tp1/Example1/tp1.js
@@ -0,0 +1,191 @@
+
+// // const svg = d3.select("body")
+// //               .append("svg")
+// //               .attr("width",500)
+// //               .attr("height",500)
+// //               .style("background-color","grey")
+
+
+
+// // d3.csv("nc.csv").then(data =>{
+    
+// //     console.log(data)
+
+// // svg.selectALL("circle")
+// //    .data(data)
+// //    .enter()
+// //    .append("circle")
+// //    .attr("cx",(d,i) => i*30 +20)
+// //    .attr("cy", 50)
+// //    .attr("r",5)
+// //    .attr("fill" , d => d.Hex )
+// // });
+
+// // const svg = d3.select("body")
+// //   .append("svg")
+// //   .attr("width", 500)
+// //   .attr("height", 500)
+// //   .style("background-color", "grey");
+
+// // d3.csv("nc.csv").then(data => {
+// //   console.log("✅ Données chargées :", data);
+
+// //   svg.selectALL("circle")
+// //     .data(data)
+// //     .enter()
+// //     .append("circle")
+// //     .attr("cx", (d, i) => i * 30 + 20)
+// //     .attr("cy", 50)
+// //     .attr("r", 10)
+// //     .attr("fill", d => d.Hex);  // respecte la casse exacte du CSV
+// // });
+
+
+
+// // const svg = d3.select("body")
+// //   .append("svg")
+// //   .attr("width", 500)
+// //   .attr("height", 500)
+// //   .style("background-color", "grey");
+
+// // d3.csv("nc.csv").then(data => {
+// //   console.log(data);
+
+// //   svg.selectAll("circle")   // ✅ correct
+// //     .data(data)
+// //     .enter()
+// //     .append("circle")
+// //     .attr("cx", (d, i) => i * 30 + 20)
+// //     .attr("cy", 50)
+// //     .attr("r", 10)
+// //     .attr("fill", d => d.Hex);
+// // });
+
+
+
+
+
+
+// // const svg = d3.select("body")
+// //               .append("svg")
+// //               .attr("width",500)
+// //               .attr("height",500)
+// //               .style("background-color","grey")
+
+
+
+// // d3.csv("nc.csv").then(data =>{
+    
+// //     console.log(data)
+
+// // svg.selectAll("circle")
+// //    .data(data)
+// //    .enter()
+// //    .append("circle")
+// //    .attr("cx",(d,i) => i<15?i*30 +20:i=1)
+// //    .attr("cy", 50)
+// //    .attr("r",5)
+// //    .attr("fill" , d => d.Hex )
+// // });
+
+
+// // d3.csv("nc.csv").then(data => {
+// //   const svg = d3.select("body")
+// //                 .append("svg")
+// //                 .attr("width", 500)
+// //                 .attr("height", 500)
+// //                 .style("background-color", "lightgrey");
+
+// //   const groups = chunkArray(data, 15);  // Découpe le CSV en groupes de 15
+
+// //   groups.forEach((group, rowIndex) => {
+// //     svg.selectAll(`circle.row${rowIndex}`)
+// //        .data(group)
+// //        .enter()
+// //        .append("circle")
+// //        .attr("cx", (d, i) => i * 30 + 20)         // position X
+// //        .attr("cy", rowIndex * 30 + 30)            // position Y par ligne
+// //        .attr("r", 10)
+// //        .attr("fill", d => d.Hex);
+// //   });
+// // });
+
+// const svg = d3.select("body")
+//   .append("svg")
+//   .attr("width", 500)
+//   .attr("height", 500)
+//   .style("background-color", "white");
+
+// d3.csv("nc.csv").then(data => {
+//   console.log("✅ Données chargées :", data);
+
+//   const circlesPerRow = 15;   // nombre de cercles par ligne
+//   const spacing = 30;         // distance entre cercles
+
+//   svg.selectAll("circle")
+//     .data(data)
+//     .enter()
+//     .append("circle")
+//     .attr("cx", (d, i) => (i % circlesPerRow) * spacing + 20) // colonne
+//     .attr("cy", (d, i) => Math.floor(i / circlesPerRow) * spacing + 20) // ligne
+//     .attr("r", 10)
+//     .attr("fill", d => d.Hex);
+// });
+
+
+// const svg1 = d3.select("body")
+//                .append("svg")
+//                .attr("width",200)
+//                .attr("height",200)
+//                .style("background-color","grey")
+
+// d3.csv("nc.csv").then(data => {
+
+//     svg1.selectAll("circle")
+//         .data(data)
+//         .enter()
+//         .append("circle")
+//         .attr("cx",(d,i) => i*20)
+//         .attr("cy",(d,i) => i*20)
+//         .attr("r",7)
+//         .attr("fill",d => d.Hex)
+
+
+// })
+
+
+
+const svg = d3.select("body")
+              .append("svg")
+              .attr("width",800)
+              .attr("height",800)
+              .style("background-color","grey")
+
+d3.csv("nc.csv").then(data => {
+
+    let x=30
+    let y=20
+    data.forEach((d,i) => {
+        
+       svg.append("circle")
+       .attr("cx",x)
+       .attr("cy",y)
+       .attr("r",15)
+       .attr("fill",d.Hex)
+
+       svg.append("text")
+          .attr("x",x-15)
+          .attr("y",y+25)
+          .text(d.Name)
+          .style("font-size","12px")
+
+
+       x=x+90;
+       if(x>700){
+        x=30;
+        y=y+80;
+       }
+
+    });
+    
+});
