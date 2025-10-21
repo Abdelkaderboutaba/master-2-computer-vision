a6a4b5608ef7e605d674ac6c9d52b45326154ef6
abdelkader <boutaba.abdelkader1@gmail.com>
2025-10-21 12:32:59 +0100

    work with svg

--- visualisation-de-donnees/tp3/tp3.js
+++ visualisation-de-donnees/tp3/tp3.js
@@ -1,7 +1,7 @@
 const svg = d3.select("body")
               .append("svg")
-              .attr("height",800)
-              .attr("width",800)
+              .attr("height",1000)
+              .attr("width",1000)
               .style("background-color","grey")
 
 
@@ -13,21 +13,43 @@ d3.csv("d.csv").then(data => {
     const groupe=svg.append("g")
                 .attr("transform" , "translate(70,70)")
 
-    const xscale=d3.scaleLinear()
-               .domain(0,d3.max(data,d => d.Value))
-               .range(0,500)
+    const yscale=d3.scaleLinear()
+               .domain([0,d3.max(data,d => d.Value)])
+               .range([500,0])
 
-    const yscale=d3.scaleBand()
+    const xscale=d3.scaleBand()
                .domain(data.map(d=>d.id))
-               .range(0,500)
+               .range([0,800])
+               .padding(0.3)
     
     const axeygroupe=groupe.append("g")
-                     .attr("trnasform" , "translate(0,0)")
+                     .attr("transform" , "translate(0,0)")
                      .call(d3.axisLeft(yscale))
     const axexgroupe=groupe.append("g")
                            .attr("transform" , "translate(0,500)")
                            .call(d3.axisBottom(xscale))
-            
+
+    groupe.selectAll("rect")
+          .data(data)
+          .enter()
+          .append("rect")
+          .attr("y",d => yscale(d.Value))
+          .attr("x",d=>xscale(d.id))
+          .attr("height",d=> 500-yscale(d.Value))
+          .attr("width",xscale.bandwidth())
+          .attr("fill","red")
+    
+    groupe.append("text")
+              .attr("x",500)
+              .attr("y",500)
+              .text("(id)")
+              .attr("font-size","20px")
+    
+    groupe.append("text")
+              .attr("x",0)
+              .attr("y",0)
+              .text("(value)")
+              .attr("font-size","20px")
 })
 
 
