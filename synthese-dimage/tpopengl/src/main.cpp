4c521acb713db4c3a83456e21ea94bec69490fee
abdelkader <boutaba.abdelkader1@gmail.com>
2025-10-29 09:19:44 +0100

    work with opengl

--- synthese-dimage/tpopengl/src/main.cpp
+++ synthese-dimage/tpopengl/src/main.cpp
@@ -19,82 +19,47 @@
 //         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
         
 //         glMatrixMode(GL_PROJECTION);
-// //         glLoadIdentity();
-// //         gluPerspective(45.0, 800.0/600.0, 0.1, 100.0);
+//         glLoadIdentity();
+//         gluPerspective(45.0, 800.0/600.0, 0.1, 100.0);
         
-// //         glMatrixMode(GL_MODELVIEW);
-// //         glLoadIdentity();
-// //         glTranslatef(0.0f, 0.0f, -5.0f);
-// //         glRotatef(angle, 1.0f, 1.0f, 0.0f);
+//         glMatrixMode(GL_MODELVIEW);
+//         glLoadIdentity();
+//         glTranslatef(0.0f, 0.0f, -5.0f);
+//         glRotatef(angle, 1.0f, 1.0f, 0.0f);
         
-// //         // Dessiner un cube coloré
-// //         glBegin(GL_QUADS);
-// //             // Face avant (rouge)
-// //             glColor3f(1.0f, 0.0f, 0.0f);
-// //             glVertex3f(-1.0f, -1.0f, 1.0f);
-// //             glVertex3f( 1.0f, -1.0f, 1.0f);
-// //             glVertex3f( 1.0f,  1.0f, 1.0f);
-// //             glVertex3f(-1.0f,  1.0f, 1.0f);
+//         // Dessiner un cube coloré
+//         glBegin(GL_QUADS);
+//             // Face avant (rouge)
+//             glColor3f(1.0f, 0.0f, 0.0f);
+//             glVertex3f(-1.0f, -1.0f, 1.0f);
+//             glVertex3f( 1.0f, -1.0f, 1.0f);
+//             glVertex3f( 1.0f,  1.0f, 1.0f);
+//             glVertex3f(-1.0f,  1.0f, 1.0f);
             
-// //             // Face arrière (vert)
-// //             glColor3f(0.0f, 1.0f, 0.0f);
-// //             glVertex3f(-1.0f, -1.0f, -1.0f);
-// //             glVertex3f(-1.0f,  1.0f, -1.0f);
-// //             glVertex3f( 1.0f,  1.0f, -1.0f);
-// //             glVertex3f( 1.0f, -1.0f, -1.0f);
+//             // Face arrière (vert)
+//             glColor3f(0.0f, 1.0f, 0.0f);
+//             glVertex3f(-1.0f, -1.0f, -1.0f);
+//             glVertex3f(-1.0f,  1.0f, -1.0f);
+//             glVertex3f( 1.0f,  1.0f, -1.0f);
+//             glVertex3f( 1.0f, -1.0f, -1.0f);
             
-// //             // Face du haut (bleu)
-// //             glColor3f(0.0f, 0.0f, 1.0f);
-// //             glVertex3f(-1.0f, 1.0f, -1.0f);
-// //             glVertex3f(-1.0f, 1.0f,  1.0f);
-// //             glVertex3f( 1.0f, 1.0f,  1.0f);
-// //             glVertex3f( 1.0f, 1.0f, -1.0f);
+//             // Face du haut (bleu)
+//             glColor3f(0.0f, 0.0f, 1.0f);
+//             glVertex3f(-1.0f, 1.0f, -1.0f);
+//             glVertex3f(-1.0f, 1.0f,  1.0f);
+//             glVertex3f( 1.0f, 1.0f,  1.0f);
+//             glVertex3f( 1.0f, 1.0f, -1.0f);
             
-// //             // Face du bas (jaune)
-// //             glColor3f(1.0f, 1.0f, 0.0f);
-// //             glVertex3f(-1.0f, -1.0f, -1.0f);
-// //             glVertex3f( 1.0f, -1.0f, -1.0f);
-// //             glVertex3f( 1.0f, -1.0f,  1.0f);
-// //             glVertex3f(-1.0f, -1.0f,  1.0f);
-// //         glEnd();
+//             // Face du bas (jaune)
+//             glColor3f(1.0f, 1.0f, 0.0f);
+//             glVertex3f(-1.0f, -1.0f, -1.0f);
+//             glVertex3f( 1.0f, -1.0f, -1.0f);
+//             glVertex3f( 1.0f, -1.0f,  1.0f);
+//             glVertex3f(-1.0f, -1.0f,  1.0f);
+//         glEnd();
         
-// //         angle += 0.5f;
+//         angle += 0.5f;
         
-// //         glfwSwapBuffers(window);
-// //         glfwPollEvents();
-// //     }
-
-// //     glfwTerminate();
-// //     return 0;
-// // }
-
-
-// #include <GLFW/glfw3.h>
-// #include <iostream>
-
-// int main()
-// {
-//     // Initialisation de GLFW
-//     if (!glfwInit()) {
-//         std::cout << "Could not initialize glfw.\n";
-//         return -1;
-//     }
-
-//     // Création de la fenêtre
-//     GLFWwindow* window = glfwCreateWindow(640, 480, "OpenGL TP 1", NULL, NULL);
-//     if (!window) {
-//         glfwTerminate();
-//         return -1;
-//     }
-
-//     glfwMakeContextCurrent(window);
-
-//     // Définir la couleur de fond (R, G, B, Alpha)
-//     glClearColor(1.0f, 0.5f, 0.5f, 1.0f);
-
-//     // Boucle principale
-//     while (!glfwWindowShouldClose(window)) {
-//         glClear(GL_COLOR_BUFFER_BIT);
 //         glfwSwapBuffers(window);
 //         glfwPollEvents();
 //     }
@@ -102,6 +67,41 @@
 //     glfwTerminate();
 //     return 0;
 // }
+
+
+// // #include <GLFW/glfw3.h>
+// // #include <iostream>
+
+// // int main()
+// // {
+// //     // Initialisation de GLFW
+// //     if (!glfwInit()) {
+// //         std::cout << "Could not initialize glfw.\n";
+// //         return -1;
+// //     }
+
+// //     // Création de la fenêtre
+// //     GLFWwindow* window = glfwCreateWindow(640, 480, "OpenGL TP 1", NULL, NULL);
+// //     if (!window) {
+// //         glfwTerminate();
+// //         return -1;
+// //     }
+
+// //     glfwMakeContextCurrent(window);
+
+// //     // Définir la couleur de fond (R, G, B, Alpha)
+// //     glClearColor(1.0f, 0.5f, 0.5f, 1.0f);
+
+// //     // Boucle principale
+// //     while (!glfwWindowShouldClose(window)) {
+// //         glClear(GL_COLOR_BUFFER_BIT);
+// //         glfwSwapBuffers(window);
+// //         glfwPollEvents();
+// //     }
+
+// //     glfwTerminate();
+// //     return 0;
+// // }
 
 
 
